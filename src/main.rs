c38f793ccfc0e26c2b59b8adef2d2b00b78e2a26
yassine <yassinebenarbia980@gmail.com>
2024-04-25 17:00:42 +0200

    chor: minor commit

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use std::error::Error;
 
 mod server;
 
-// Although we use `async-std` here, you can use any async runtime of choice.
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let server = server::Server::default();
